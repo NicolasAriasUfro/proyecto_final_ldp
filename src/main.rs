377abee8bab7c42c6cf8c85b914c4462d1b0665a
NicolasAriasUfro <101823845+NicolasAriasUfro@users.noreply.github.com>
2023-07-04 11:12:20 -0400

    playground database

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 mod crypto_base;
 mod clipboard;
 mod panel;
+mod sql_playground;
 
 
 fn main() {
     println!("Hello, world!");
-    panel::panel_login();
+    //panel::panel_login();
+    sql_playground::playground();
     
     
 
---
+++ src/sql_playground.rs
@@ -0,0 +1,13 @@
+use rusqlite::{Connection,Result};
+// playground by nico
+
+pub fn playground() -> rusqlite::Result<()> {
+    let conn  = Connection::open("cat.db");
+    conn.execute(
+        "create table if not exists cat_colors(
+        id integer primary key,
+        name text not null unique
+        )",
+        [],
+    )?
+}
