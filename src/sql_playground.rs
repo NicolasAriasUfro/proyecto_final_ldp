56651de947052ffdf6d589fd35d3c9740ba6eca3
NicolasAriasUfro <101823845+NicolasAriasUfro@users.noreply.github.com>
2023-07-04 11:44:42 -0400

    with errors(link.exe, exit code:1181)

--- src/sql_playground.rs
+++ src/sql_playground.rs
@@ -1,14 +1,44 @@
-use rusqlite::{Connection, Result};
-// playground by nico por nico y solo por nico
+use rusqlite::{params, Connection, Result};
 
-pub fn playground() -> rusqlite::Result<()> {
-    let conn  = Connection::open("cat.db");
+#[derive(Debug)]
+struct Person {
+    id: i32,
+    name: String,
+    data: Option<Vec<u8>>,
+}
+
+pub fn test_2() -> Result<()> {
+    let conn = Connection::open_in_memory()?;
+
     conn.execute(
-        "create table if not exists cat_colors(
-        id integer primary key,
-        name text not null unique
+        "CREATE TABLE person (
+            id   INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            data BLOB
         )",
-        [],
-    )?
+        (), // empty list of parameters.
+    )?;
+    let me = Person {
+        id: 0,
+        name: "Steven".to_string(),
+        data: None,
+    };
+    conn.execute(
+        "INSERT INTO person (name, data) VALUES (?1, ?2)",
+        (&me.name, &me.data),
+    )?;
 
+    let mut stmt = conn.prepare("SELECT id, name, data FROM person")?;
+    let person_iter = stmt.query_map([], |row| {
+        Ok(Person {
+            id: row.get(0)?,
+            name: row.get(1)?,
+            data: row.get(2)?,
+        })
+    })?;
+
+    for person in person_iter {
+        println!("Found person {:?}", person.unwrap());
+    }
+    Ok(())
 }
