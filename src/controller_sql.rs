4f1865e6c93a62f8b27f1d2bb3e64ffffeb15143
NicolasAriasUfro <101823845+NicolasAriasUfro@users.noreply.github.com>
2023-07-10 13:58:27 -0400

    funcion agregar cuenta

--- src/controller_sql.rs
+++ src/controller_sql.rs
@@ -1,19 +1,21 @@
 use rusqlite::{params, Connection, Result};
+use crate::controlador::manipular_info::info_almacenada::Entrada;
 
-pub fn set_database() {
+pub fn set_database() -> Result<()> {
     let conn = Connection::open("database.db")?;
-    //crear la tabla de passwords
+    //crear la tabla de cuentas
     conn.execute(
-        "create table cuentas IF NOT EXISTS(
-        id            integer primary key autoincrement ,
-        title         TEXT,
-        hash_password TEXT NOT NULL,
-        user_name     TEXT NOT NULL,
-        url           TEXT,
-        desc_password TEXT NOT NULL
-    )",
-        (),
+        "CREATE TABLE IF NOT EXISTS cuentas (
+            id            INTEGER PRIMARY KEY AUTOINCREMENT,
+            title         TEXT,
+            user_name     TEXT NOT NULL,
+            hash_password TEXT NOT NULL,
+            url           TEXT,
+            desc_password TEXT NOT NULL
+        )",
+        params![],
     )?;
+    Ok(())
     //TODO: agregar tabla que verifique la contraseña usada para entrar al programa.
 }
 
@@ -33,4 +35,13 @@ pub fn update_column() {}
 
 pub fn delete_column() {}
 
-//pub fn agregar_cuenta($cuenta:cuenta)
+
+pub fn agregar_cuenta(cuenta:&Entrada) -> Result<()>{
+    let conn = Connection::open("database.db")?;
+    conn.execute(
+        "INSERT INTO cuentas (title,user_name,hash_password,url)
+            Values(?1,?2,V3)",
+        params![cuenta.titulo,cuenta.nombre_usuario,cuenta,cuenta.hash_password,cuenta.url]
+    )?;
+    Ok(())
+}
