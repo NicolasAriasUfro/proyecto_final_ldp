f89f9519454e6ef88cd88462702950db872e8178
Manuel <72231982+zendar426@users.noreply.github.com>
2023-07-11 19:37:17 -0400

    loop

--- src/panel.rs
+++ src/panel.rs
@@ -26,11 +26,9 @@ pub fn panel_register() {
 
 //panel principal de la aplicación, muestra todas las cuentas almacenadas con su titulo (si hay), user, url(si hay) y password
 pub fn panel_main() {
-    loop{
+    loop {
         seleccionar();
     }
-
-    
 
     //el panel main debe manejar alguna estructura de datos que contenga las cuentas, este obviamente debe ser poblado mediante instrucciones
     //SELECT , luego se recorre toda la estructura de datos y se imprimen las cuentas con su numero
@@ -51,11 +49,7 @@ fn sort_by_title() {
 }
 
 pub fn seleccionar() -> std::io::Result<()> {
-    let items = vec![
-        "Ver cuentas",
-        "Crear cuenta nueva",
-        "Borrar cuenta",
-    ];
+    let items = vec!["Ver cuentas", "Crear cuenta nueva", "Borrar cuenta"];
     println!("para salir presione esc");
     let selection = Select::with_theme(&ColorfulTheme::default())
         .with_prompt("Menu")
@@ -63,11 +57,10 @@ pub fn seleccionar() -> std::io::Result<()> {
         .default(0)
         .interact_on_opt(&Term::stderr())?;
 
-
     match selection {
         Some(index) => {
             if index == 0 {
-                vista_for_selection();
+                vista_for_selection()?;
             }
             if index == 1 {
                 println!("agua")
@@ -75,7 +68,6 @@ pub fn seleccionar() -> std::io::Result<()> {
             if index == 2 {
                 println!("tierra")
             }
-            
         }
         None => {
             println!("Saliendo");
@@ -87,16 +79,20 @@ pub fn seleccionar() -> std::io::Result<()> {
 }
 
 fn vista_for_selection() -> std::io::Result<()> {
-    
     let default_choice_for_sort = false;
 
-
-    let cuentas = vec![
-        "Fortnite              daddyisues             null            23/07/2023",
-    ];
+    let mut cuentas = Vec::new();
+    let id: String = "1".to_string();
+    let title: String = "Fornite".to_string();
+    let user: String = "abzassssdsdsds".to_string();
+    let url: String = "www.epicgames.com".to_string();
+    let password: String = "*********".to_string();
+    let date: String = "23/07/2023".to_string();
+    let string_pusher = format!("{:<8} {:<8} {:<8} {:<8} {:<8} {:<8}", id, title, user, url, password, date);
+    cuentas.push(string_pusher);
 
     let selection = Select::with_theme(&ColorfulTheme::default())
-        .with_prompt("Titulo                    usuario                 Contraseña                  Url                 Fecha")
+        .with_prompt("Titulo            Usuario             Url                 Contraseña          fecha   ")
         .items(&cuentas)
         .default(0)
         .interact_on_opt(&Term::stderr())?;
@@ -106,23 +102,17 @@ fn vista_for_selection() -> std::io::Result<()> {
             println!("todo");
         }
         None => {
-                    println!("Regresando")
-                }
+            println!("Regresando")
+        }
     }
     Ok(())
-
-    /*
-    Ejemplo de vista del panel
-    CUENTAS:
-    1 titulo   user         url                       password
-    2 google   juan         https://www.google.com    ********
-    3          a@gmail.com                            ********
-
-    crear nueva cuenta: q borrar una cuenta:w salir: e
-     */
 }
 
 fn vista_for_delete() {}
+
+fn vista_for_update() {
+    todo!()
+}
 
 // este panel debe mostrar el contenido de la contraseña una vez se escoja una
 pub fn panel_contenido() {
